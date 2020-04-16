38ec3bd6387f999c8e997ea4899798fce97e516e
Riadh MNASRI <riadh.mnasri@gmail.com>
2020-04-16 13:00:58 +0200

    Add project skeleton

---
+++ src/main/java/com/riadh/gameoflife/GameOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.riadh.gameoflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riadh/gameoflife/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riadh.gameoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
